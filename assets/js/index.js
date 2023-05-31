3ce0f624b640e0ea92a332479854bd9da0114247
mkarsums15@mail.ru <mkarsums15@mail.ru>
2023-05-31 20:25:20 +0300

    add timers

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,19 @@
-let button = document.querySelector(".button");
-let email = document.querySelector("#email");
-let password = document.querySelector("#password");
-let firstName = document.querySelector("#first-name");
-let lastName = document.querySelector("#last-name");
-let age = document.querySelector("#age");
-let pattern = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
-let valid = pattern.test(email.value);
-let clearMessageEmpty = document.querySelectorAll(".form__label-empty");
-let clearMessageError = document.querySelectorAll(".form__label-error");
+const button = document.querySelector(".button");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const firstName = document.querySelector("#first-name");
+const lastName = document.querySelector("#last-name");
+const age = document.querySelector("#age");
+const pattern = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
+const valid = pattern.test(email.value);
+const inputs = document.querySelectorAll(".form__input");
+const checkboxes = document.querySelectorAll(".form__chekbox");
+const clearMessageEmpty = document.querySelectorAll(".form__label-empty");
+const clearMessageError = document.querySelectorAll(".form__label-error");
+const userDataField = document.querySelector(".user-info");
+const user = [];
+let userTime = setTimeout(remind, 7000);
+let i = 0;
 
 button.addEventListener("click", function checkAccess() {
 	let correctInput = 0;
@@ -26,22 +32,34 @@ button.addEventListener("click", function checkAccess() {
 	} else if (pattern.test(email.value) == false && email.value != "") {
 		document.querySelector("#emailError").classList.remove("inactive");
 		document.querySelector("#emailError").classList.add("active");
-	} else correctInput++;
+	} else {
+		user.push(email.value);
+		correctInput++;
+	}
 
 	if (password.value == "") {
 		document.querySelector("#passwordEmpty").classList.remove("inactive");
 		document.querySelector("#passwordEmpty").classList.add("active");
-	} else correctInput++;
+	} else {
+		user.push(password.value);
+		correctInput++;
+	}
 
 	if (firstName.value == "") {
 		document.querySelector("#firstNameEmpty").classList.remove("inactive");
 		document.querySelector("#firstNameEmpty").classList.add("active");
-	} else correctInput++;
+	} else {
+		user.push(firstName.value);
+		correctInput++;
+	}
 
 	if (lastName.value == "") {
 		document.querySelector("#lastNameEmpty").classList.remove("inactive");
 		document.querySelector("#lastNameEmpty").classList.add("active");
-	} else correctInput++;
+	} else {
+		user.push(lastName.value);
+		correctInput++;
+	}
 
 	if (age.value == "") {
 		document.querySelector("#ageEmpty").classList.remove("inactive");
@@ -49,9 +67,42 @@ button.addEventListener("click", function checkAccess() {
 	} else if (isNaN(age.value) && age.value != "") {
 		document.querySelector("#ageError").classList.remove("inactive");
 		document.querySelector("#ageError").classList.add("active");
-	} else correctInput++;
+	} else {
+		user.push(age.value);
+		correctInput++;
+	}
 
 	if (correctInput == document.querySelectorAll(".form__label").length) {
 		document.querySelector(".welcome-input").innerHTML = `Добро пожаловать, ${firstName.value}!`;
+		showData(user);
 	}
 });
+
+// timer
+
+inputs.forEach((input) => {
+	input.addEventListener("keyup", updateActivity);
+});
+
+checkboxes.forEach((checkbox) => {
+	checkbox.addEventListener("click", updateActivity);
+});
+
+function showData(user) {
+	let timerId = setInterval(function () {
+		let div = document.createElement("div");
+		div.classList.add("user-info_element");
+		let info = user[i++] || clearInterval(timerId);
+		div.innerHTML = info;
+		if (info !== undefined) userDataField.append(div);
+	}, 3000);
+}
+
+function remind() {
+	alert("Что-то долго думаете");
+}
+
+function updateActivity() {
+	clearTimeout(userTime);
+	userTime = setTimeout(remind, 7000);
+}
